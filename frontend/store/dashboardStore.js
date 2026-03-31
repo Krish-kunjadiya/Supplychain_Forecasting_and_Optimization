d31164307793a68517dc6d7e6c2249e9a628f96d
Krish-kunjadiya <krishkunjadiya1@gmail.com>
2026-04-01 00:49:15 +0530

    Revert "Next Js Dashboard"
    This reverts commit 888a05730283455bd75c6f8cbbd96d361d04ec9c.

--- frontend/store/dashboardStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-export const useDashboardStore = create((set) => ({
-  activeJobId: null,
-  selectedSku: null,
-  setJobId: (id) => set({ activeJobId: id }),
-  setSelectedSku: (sku) => set({ selectedSku: sku })
-}));
